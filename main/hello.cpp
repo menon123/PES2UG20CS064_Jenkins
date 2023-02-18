56a27d75e781a4fd606bd6160740e52af66d96ab
Arati Menon <menon_arati@yahoo.com>
2023-02-18 09:56:37 +0530

    task2

--- main/hello.cpp
+++ main/hello.cpp
@@ -4,7 +4,6 @@ using namespace std;
 
 int main(void) {
   cout << "Hello, World\n";
-  cout << "Hello, Everybody\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run\n";
   return 0;
